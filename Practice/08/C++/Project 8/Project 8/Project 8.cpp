4b2b96caf1f253176ed943aa62f29abf3492e950
VsevolodShengelai <famy@mail.ua>
2020-10-29 00:53:17 +0300

    Tasks 10 - 15 on C++

---
+++ Practice/08/C++/Project 8/Project 8/Project 8.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+#include <clocale> 
+
+using namespace std;
+
+void getValue (double *a, double *b, char *operation)
+{
+    while (true) // цикл продолжается до тех пор, пока пользователь не введет корректное значение
+    {
+        std::cout << "Bведите выражение" <<endl;
+        std::cin >> *a >> *operation >> *b;
+
+        // Проверка на предыдущее извлечение
+        if (std::cin.fail()) // если предыдущее извлечение оказалось неудачным,
+        {
+            std::cin.clear(); // то возвращаем cin в 'обычный' режим работы
+            std::cin.ignore(32767, '\n'); // и удаляем значения предыдущего ввода из входного буфера
+            std::cout << "Ввод некорректен. Попробуйте ещё раз\n";
+            continue;
+        }
+        else
+        {
+            std::cin.ignore(32767, '\n'); // удаляем лишние значения
+
+            return;
+        }
+    }
+}
+double Calculate(double a, double b, char operation)
+{
+    switch (operation)
+    {
+    case '+':
+        cout << a + b;
+        return true;
+        break;
+    case '*':
+        cout << a * b;
+        return true;
+        break;
+    case '-':
+        cout << a - b;
+        return true;
+        break;
+    case '/':
+        if (b == 0)
+        {
+            cerr << "На 0 делить нельзя \n";
+            return false;
+        }
+        else
+        {
+            cout << a / b;
+            return true;
+            break;
+        }
+
+    default:
+        cout << "Этот оператор недопустим" << endl;
+        return false;
+        break;
+    }
+}
+
+int main() 
+{
+    setlocale(LC_ALL, "Russian");
+
+	cout << "Консольное приложение Калькулятор" << endl << endl;
+	cout << "Введите операцию для выполнения. Формат: a + b | a - b | a * b | a / b " << endl;
+
+    double a, b; char operation;
+    getValue(&a, &b, &operation);
+
+    // bool flag = 
+    Calculate(a, b, operation);
+
+}
+
+
+
---
+++ Practice/13/C++/Project 13/Project 13.1/Project 13.1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <clocale> 
+using namespace std;
+
+/*
+Данная программа реализует Алгоритм Эрастрофена, называемый иногда "Решетом Эрастрофена"
+*/
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int i, n;
+    cout << "Введите число\n";
+    cin >> n;
+    bool flag;
+    flag = true;
+    for (i = 2; i < sqrt(n); i++)
+        if (n % i == 0)
+        {
+            cout << "Составное";
+            flag = false;
+        }
+    if (flag)
+        cout << "Простое";
+     
+}
