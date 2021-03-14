005d19f8b3bfad173ff01c9bc03471251d5c9f91
VsevolodShengelai <famy@mail.ua>
2021-03-14 23:45:12 +0300

    _

---
+++ Practice/34/С++/Task_34/Task 34/Task 34.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int* create(int** arr, int n, int start= 0, int step = 0)
+{
+    int* a = new int[n];
+
+    for (int i = 0; i < n; i = i ++)
+    {
+        a[i] = start;
+        start += step;
+        //cout << a[i] << endl;
+    }
+
+    return a;
+}
+
+int destroy(int** arr)
+{
+    if (arr == nullptr)
+    {
+        return 0;
+    }
+    if (*arr)
+    {
+        delete[] * arr;
+        *arr = nullptr;
+    }
+    return 0;
+}
+
+void sort(int* array, int length) {
+    for (int i = 1; i < length; i++) {
+        for (int j = i; j > 0 && array[j - 1] > array[j]; j--) {
+            int tmp = array[j - 1];
+            array[j - 1] = array[j];
+            array[j] = tmp;
+        }
+    }
+}
+
+int* print(int* array, int length)
+{
+    std::cout << '[';
+    for (int i = 0; i < length; i++)
+    {
+        std::cout << array[i];
+
+        if (i < length - 1)
+        {
+            std::cout << ", ";
+        }
+    }
+
+    std::cout << ']' << std::endl;
+
+    return array;
+}
+
+int main()
+{
+    int n, start, step;
+
+    std::cin >> n >> start >> step;
+
+    int* arr = create(&arr, n, start, step);
+
+    sort(arr, n);
+    print(arr, n);
+
+    destroy(&arr);
+}
---
+++ Practice/35/C++/Task_35/Task_35/Task_35.cpp
@@ -0,0 +1,117 @@
+﻿#include <iostream>
+
+struct IntArray {
+    int* data;
+    int size;
+};
+
+void create(IntArray* arr, int size) {
+    arr->data = new int[size]();
+    arr->size = size;
+}
+
+void create(IntArray& arr, int size) {
+    arr.data = new int[size]();
+    arr.size = size;
+}
+
+int get(IntArray& arr, int index)
+{
+    if (index < 0 || index >= arr.size){
+        std::cout << "Ошибка, выхрод за границу массива. Работа программы завершена";
+        exit(1);
+    }
+    return arr.data[index];
+}
+int get(IntArray* arr, int index)
+{
+    return get(*arr, index);
+}
+
+void set(IntArray& arr, int index, int value)
+{
+    if (index < 0 || index >= arr.size){
+        std::cout << "Ошибка, выхрод за границу массива. Работа программы завершена";
+        exit(1);
+    }
+    arr.data[index] = value;
+}
+void set(IntArray* arr, int index, int value)
+{
+    set(*arr, index, value);
+}
+
+void resize(IntArray* arr, int newSize) {
+    int* newArr = new int[newSize];
+
+    if (newSize < arr->size) {
+        for (int i = 0; i < newSize; i++)
+            newArr[i] = arr->data[i];
+    }
+    else { // newSize >= arr->size
+        for (int i = 0; i < arr->size; i++)
+            newArr[i] = arr->data[i];
+        for (int i = arr->size; i < newSize; i++)
+            newArr[i] = 0;
+    }
+
+    delete arr->data;
+    arr->data = newArr;
+    arr->size = newSize;
+}
+void resize(IntArray& arr, int newSize)
+{
+    resize(&arr, newSize);
+}
+
+void destroy(IntArray& arr)
+{
+    if (!arr.data)
+    {
+        delete[] arr.data;
+        arr.data = nullptr;
+    }
+    arr.size = 0;
+}
+void destroy(IntArray* arr)
+{
+    destroy(*arr);
+}
+
+void print(IntArray& arr)
+{
+    if (arr.size == 0)
+    {
+        puts("[]");
+        return;
+    }
+    if (arr.size < 0)
+    {
+        return;
+    }
+    printf("[%d", get(arr, 0));
+    for (int i = 1; i < arr.size; ++i)
+        printf(", %d", get(arr, i));
+    printf("]\n");
+}
+void print(IntArray* arr)
+{
+    print(*arr);
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    
+    IntArray arr = {};
+    create(arr, 30);
+    for (int i = 0; i < 30; i++) {
+        set(arr, i, i + 1);
+    }
+    print(arr);
+    resize(arr, 50);
+    print(arr);
+    resize(arr, 10);
+    print(arr);
+    destroy(arr);
+}
